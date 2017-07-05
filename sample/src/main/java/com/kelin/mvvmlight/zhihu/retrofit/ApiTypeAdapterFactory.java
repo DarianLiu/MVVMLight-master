6366be333a80170a7049a488088d881f19cec925
Darian <darian.liu@foxmail.com>
2017-07-05 09:28:06 +0800

    Initial commit 新建项目

---
+++ sample/src/main/java/com/kelin/mvvmlight/zhihu/retrofit/ApiTypeAdapterFactory.java
@@ -0,0 +1,63 @@
+package com.kelin.mvvmlight.zhihu.retrofit;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ * Created by dingzhihu on 15/5/7.
+ */
+public class ApiTypeAdapterFactory implements TypeAdapterFactory {
+    private String dataElementName;
+
+    public ApiTypeAdapterFactory(String dataElementName) {
+        this.dataElementName = dataElementName;
+    }
+
+    @Override
+    public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+        final TypeAdapter<T> delegate = gson.getDelegateAdapter(this, type);
+        final TypeAdapter<JsonElement> elementTypeAdapter = gson.getAdapter(JsonElement.class);
+
+
+        return new TypeAdapter<T>() {
+            @Override
+            public void write(JsonWriter out, T value) throws IOException {
+                delegate.write(out, value);
+            }
+
+            @Override
+            public T read(JsonReader in) throws IOException {
+                JsonElement jsonElement = elementTypeAdapter.read(in);
+                if (jsonElement.isJsonObject()) {
+                    JsonObject jsonObject = jsonElement.getAsJsonObject();
+                    if (jsonObject.has("status")) {
+                        int status = jsonObject.get("status").getAsInt();
+                        String message = jsonObject.get("message").getAsString();
+                        if (status == 0) {
+                            Log.e("===请求回调状态==","0");
+                        } else {
+                            Log.e("===请求回调状态==","抛出异常");
+                            throw new ApiException(status, message);
+                        }
+                    }
+                    if (jsonObject.has(dataElementName)) {
+                        jsonElement = jsonObject.get(dataElementName);
+                    }
+                }
+                return delegate.fromJsonTree(jsonElement);
+            }
+
+        }.nullSafe();
+    }
+}
